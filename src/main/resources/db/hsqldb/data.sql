7de214dff05201d89da930ffe3bed4502bc0944b
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2021-11-24 13:29:36 +0100

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -5,16 +5,36 @@ INSERT INTO achievements(id, name, description) VALUES(3, 'NombreAchievement3', 
 INSERT INTO users(username, password, enabled) VALUES('ManuK', 1234567, 'true');
 INSERT INTO users(username, password, enabled) VALUES('marioespiro', 'admin121', 'true');
 INSERT INTO users(username, password, enabled) VALUES('pedro', 'pedro121', 'true');
+INSERT INTO users(username, password, enabled) VALUES('jaime', 'jaime', 'true');
+INSERT INTO users(username, password, enabled) VALUES('jose', 'jose', 'true');
+INSERT INTO users(username, password, enabled) VALUES('antonio', 'antonio', 'true');
+INSERT INTO users(username, password, enabled) VALUES('paco', 'paco', 'true');
 
 INSERT INTO players(id, email, username) VALUES(1, 'manu@gmail.com', 'ManuK');
 INSERT INTO players(id, email, username) VALUES(2, 'mario@testmail.com', 'marioespiro');
 INSERT INTO players(id, email, username) VALUES(3, 'pedro@pedromail.com', 'pedro');
+INSERT INTO players(id, email, username) VALUES(4, 'jaime@domain.com', 'jaime');
+INSERT INTO players(id, email, username) VALUES(5, 'jose@domain.com', 'jose');
+INSERT INTO players(id, email, username) VALUES(6, 'antonio@domain.com', 'antonio');
+INSERT INTO players(id, email, username) VALUES(7, 'paco@domain.com', 'paco');
 
 INSERT INTO authorities(id, authority, username) VALUES(1, 'admin', 'ManuK');
 INSERT INTO authorities(id, authority, username) VALUES(2, 'admin', 'marioespiro');
 INSERT INTO authorities(id, authority, username) VALUES(3, 'player', 'pedro');
+INSERT INTO authorities(id, authority, username) VALUES(4, 'player', 'jaime');
+INSERT INTO authorities(id, authority, username) VALUES(5, 'player', 'jose');
+INSERT INTO authorities(id, authority, username) VALUES(6, 'player', 'antonio');
+INSERT INTO authorities(id, authority, username) VALUES(7, 'player', 'paco');
+
+INSERT INTO goose_matches(id, end_date, match_code, start_date) VALUES(1, curdate(), null, null);
+
+INSERT INTO PLAYER_GOOSE_STATS(id, double_rolls, landed_bridges, landed_dice, landed_geese, landed_jails, landed_specials, goose_match_id, player_id) VALUES(1,2,3,4,5,6,7,1,4);
+INSERT INTO PLAYER_GOOSE_STATS(id, double_rolls, landed_bridges, landed_dice, landed_geese, landed_jails, landed_specials, goose_match_id, player_id) VALUES(2,2,3,4,5,6,7,1,5);
+INSERT INTO PLAYER_GOOSE_STATS(id, double_rolls, landed_bridges, landed_dice, landed_geese, landed_jails, landed_specials, goose_match_id, player_id) VALUES(3,2,3,4,5,6,7,1,6);
+INSERT INTO PLAYER_GOOSE_STATS(id, double_rolls, landed_bridges, landed_dice, landed_geese, landed_jails, landed_specials, goose_match_id, player_id) VALUES(4,2,3,4,5,6,7,1,7);
 
 INSERT INTO achievements_players(achievements_id, players_id) VALUES(2, 1);
 INSERT INTO achievements_players(achievements_id, players_id) VALUES(3, 1);
+
 
 
