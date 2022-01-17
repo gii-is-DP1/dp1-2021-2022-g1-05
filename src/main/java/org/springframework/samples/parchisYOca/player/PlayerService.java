e97f1ffdcaa2ae3df044bd8fca5b8afe6577178d
Manuel Padilla Tabuenca <mpadillatabuenca@gmail.com>
2022-01-17 12:28:24 +0100

    Tests de servicio achievement y player

--- src/main/java/org/springframework/samples/parchisYOca/player/PlayerService.java
+++ src/main/java/org/springframework/samples/parchisYOca/player/PlayerService.java
@@ -61,7 +61,7 @@ public class PlayerService {
         Player playerDB;
         //Check if player already exists (in other words, if this is and edit instead of a creation)
         //and if so, set the username so that it matches the current one, just in case
-        if( player.getId()!=null) {
+        if(player.getId()!=null) {
             User userIntroduced = player.getUser();
             userIntroduced.setUsername(playerRepository.findById(player.getId()).get().getUser().getUsername());
             player.setUser(userIntroduced);
